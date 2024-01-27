b056d76213aac5c50b107ef1ea0f4f53cef0d04c
Andy Van Slaars <avanslaars@gmail.com>
2024-01-27 15:20:05 -0500

    updated default index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,22 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import type { MetaFunction } from '@remix-run/cloudflare'
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
+  ]
+}
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <h1>Welcome to my Remix Sandbox</h1>
+      <p>
+        I use Remix at work, but need a place to play around where I don&apos;t
+        have to follow anyone&apos;s styling, requirements, or CI/CD
+        shenanigans. Using Cloudflare pages should make it nice and easy to mess
+        around with things and put it out there for easy sharing.
+      </p>
     </div>
-  );
+  )
 }
